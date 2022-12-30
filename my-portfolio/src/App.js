3b44c2514d2f24d27a91961a5b8106b391018964
Miguel Angel Rivas <miguel.angel.rivas@consid.se>
2022-12-30 08:27:41 +0100

    upload react project

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <header>
+        <div>
+          <nav className="flex w-full justify-evenly ">
+            <a>about </a>{' '}
+            <a>project </a>{' '}
+            <a>contact</a>
+          </nav>
+        </div>
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
