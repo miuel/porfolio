c6069e0994d6ac2cf8ba040c0230c24588979b69
Miguel Rivas <rivasmig@gmail.com>
2023-01-08 17:37:28 +0100

    created grid-col-custom css class

--- my-portfolio/src/pages/Projects.js
+++ my-portfolio/src/pages/Projects.js
@@ -14,9 +14,20 @@ const ProjectPage = () => {
       <h1 className="text-6xl line-height-1-6 font-bold font-bodoni py-5">
         Projects
       </h1>
-      <div className="grid grid-cols-2 gap-4 h-664">
+
+
+      <h1 className="text-xl line-height-1-6 font-bold font-bodoni py-5">
+        Mobile
+      </h1>
+
+      <div className="grid-col-custom">
+        <img src={data.ospuncba} className="w-auto " />
+        <img src={data.wallet} className="w-auto rounded-md border-stone-400 border " />
+      </div>
+
+      <div className="mt-5 h-56 sm:h-64 xl:h-80 2xl:h-96">
         <Carousel>
-          {/* <img
+          <img
             src={data.intro}
             alt="..."
             className="object-contain"
@@ -36,12 +47,8 @@ const ProjectPage = () => {
           <img
             src={data.grid}
             alt="..."
-          /> */}
-          <img src={data.ospuncba} className="w-auto " />
-        </Carousel>
-        <Carousel indicators={false} >
-          <img src={data.wallet} className="w-auto " />
-          
+          />
+
         </Carousel>
       </div>
     </section>
