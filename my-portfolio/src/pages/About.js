5ce6bfaf7961e7eff19e2aa0a7a66d1fcbe41d8a
Miguel Rivas <rivasmig@gmail.com>
2023-01-30 10:15:00 +0100

    added process info

--- my-portfolio/src/pages/About.js
+++ my-portfolio/src/pages/About.js
@@ -34,10 +34,23 @@ const AboutPage = () => {
       <article className="flex p-10 gap-8">
         <div className="w-1/3">
           <h1>{data.processOfCreation.heading}</h1>
+          <h2 className="text-xl" >{data.processOfCreation.service}</h2>
         </div>
 
         <div className="w-2/3">
-         
+          <ul>
+          <p className="paragraph-style text-85rgba">Process</p>
+            {data.processOfCreation.process && data.processOfCreation.process.map((process, index) => (
+              <li key={index.toString()} className="py-4">
+                <h3 className="text-2xl">0{index + 1} ⟛ <i>
+                  {process.subheading}
+                  </i>
+                  </h3>
+                <p className="paragraph-style text-85rgba">{process.description}</p>
+              </li>
+
+            ))}
+          </ul>
         </div>
       </article>
     </section>
