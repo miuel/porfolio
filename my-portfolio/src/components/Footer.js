d263e559c60f30fb28eb88cfc1c14dce86b6101c
Miguel Rivas <rivasmig@gmail.com>
2023-07-07 16:14:46 +0200

    become footer to marquee

--- my-portfolio/src/components/Footer.js
+++ my-portfolio/src/components/Footer.js
@@ -6,18 +6,29 @@ import Marquee from "../components/Marquee";
 const Footer = () => {
 
   return (
-    <footer className="w-full py-10 gap-8">
+    <footer className="w-full pt-10 gap-8">
       <Marquee
         data={data.marqueeHeaderText}
         repeatTimes={3}
         bgColor="bg-stone-400"
-        height={"h-14"}
-        fontSize={"text-2xl"}
+        height={"h-[100px]"}
+        fontSize={"text-4xl "}
         showBullet={true}
       />
-      <div className="flex ">
 
-        {data.footer.map((item, i) => (
+
+
+      <Marquee
+        data={data.footer}
+        repeatTimes={5}
+        bgColor="bg-black"
+        fontSize={"text-xl font-bold"}
+        height={"h-[80px]"}
+        showBullet
+      />
+
+
+      {/* {data.footer.map((item, i) => (
           <div key={i.toString()} className="max-w-75 flex flex-col justify-center" >
             <h3 className="font-bold grid-cols-1 pl-4 font-monospace">
               {item.title}
@@ -26,8 +37,8 @@ const Footer = () => {
               {item.data}
             </p>
           </div>
-      ))}
-      </div>
+        ))}  */}
+
     </footer>
   );
 };
