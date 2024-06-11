d695e8fa313f9b4f7796287c0f5d427f5cbde167
Miguel Rivas <rivasmig@gmail.com>
2024-06-11 13:45:12 +0200

    use gradient bg + added Icon logo

--- my-portfolio/src/components/Navigation.js
+++ my-portfolio/src/components/Navigation.js
@@ -2,9 +2,10 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, NavLink } from "react-router-dom";
 import data from "../api/data-mock.json";
-import Button from "./Button";
-import useImagePath from "../hooks/useImagePath";
 
+import useImagePath from "../hooks/useImagePath";
+import Icon from "../components/Icon";
+import Marquee from "../components/Marquee";
 
 const Navigation = () => {
   const location = useLocation();
@@ -40,26 +41,36 @@ const Navigation = () => {
     setSelectedOption(pageSelected)
   }
 
+
   return (
 
     <nav
-      className={`bg-orange-400 sticky top-0 z-10 md:mb-20 shadow-lg ${!isScrolling ? 'opacity-100 transition-all duration-300' : ' opacity-0 transition-all duration-300'}`}
+      className={`animated-gradient sticky top-0 z-10 md:mb-20 shadow-lg ${!isScrolling ? 'opacity-100 transition-all duration-300' : ' opacity-0 transition-all duration-300'}`}
     >
+      {/* <Marquee
+        data={data.footer}
+        repeatTimes={3}
+        showBullet={true}
+        isLevel1
+        className="md:mb-5"
+      /> */}
       <div className="max-w-6xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <NavLink to="/" exact="true">
-          <img src={imageSrc} alt="Miguel Rivas Logo" className="w-36 md:w-24" />
-        </NavLink>
+        {imageSrc &&
+          <NavLink to="/" exact="true">
+            <Icon icon="logoOfficial" alt="Miguel Angel Logo" size={8} className="text-black opacity-70 mix-blend-multiply w-36 md:w-24 " />
+          </NavLink>
+        }
 
 
         {isMobile && (
           <button
             type="button"
-            className="text-white hover:text-white focus:outline-none"
+            className="text-black hover:text-black focus:outline-none"
             onClick={() => setShowMenu(!showMenu)}
           >
             <span className="sr-only">Open main menu</span>
             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
             </svg>
           </button>
         )
@@ -69,7 +80,7 @@ const Navigation = () => {
           className={`w-full md:w-auto md:flex flex-grow ${showMenu ? 'block ease-out ' : 'hidden'} md:block`}
         >
           <ul className="w-full flex flex-col items-end mt-8 md:mt-0 md:flex-row justify-end gap-2 md:gap-6">
-            {data.navigation.map((option, index) => (
+            {data?.navigation.map((option, index) => (
               <NavLink
                 to={option.url}
                 className={`text-sm w-fit hover:bg-white px-1 ${option.url === selectedOption ? "bg-white" : ""}`}
@@ -83,6 +94,7 @@ const Navigation = () => {
           </ul>
         </div>
       </div>
+
     </nav>
 
 
