d619b2656be7fcb4bd8b96272b79d520a6d11fa1
Miguel Rivas <rivasmig@gmail.com>
2024-06-13 15:48:16 +0200

    unmount TextShuffleComponent when is not used.

--- my-portfolio/src/components/TextShuffle.js
+++ my-portfolio/src/components/TextShuffle.js
@@ -3,21 +3,29 @@ import { shuffle } from 'txt-shuffle';
 
 const TextShuffleComponent = ({ text, glyphs, duration }) => {
     const textRef = useRef(null);
+    const isMounted = useRef(true);
 
     useEffect(() => {
+        isMounted.current = true;
+
         if (textRef.current) {
             shuffle({
                 text: text,
                 duration: duration,
                 fps: 20,
                 glyphs: glyphs,
-                //glyphs: "_",
                 onUpdate: (output) => {
-                    textRef.current.textContent = output;
+                    if (isMounted.current && textRef.current) {
+                        textRef.current.textContent = output;
+                    }
                 },
             });
         }
-    }, [text]);
+
+        return () => {
+            isMounted.current = false;
+        };
+    }, [text, glyphs, duration]);
 
     return <div ref={textRef}>{text}</div>;
 };
