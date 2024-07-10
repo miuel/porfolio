4141fa898c7794b774a1d62fbef79c85987c6540
Miguel Angel Rivas <miguel.angel.rivas@consid.se>
2024-07-10 14:03:01 +0200

    added new spline link

--- my-portfolio/src/pages/Lab.js
+++ my-portfolio/src/pages/Lab.js
@@ -30,7 +30,8 @@ const LabPage = () => {
           <h1>{data?.lab.title}</h1>
           <div className="grid md:grid-cols-2 mt-5 py-4 border-b border-stone-400 md:border-none ">
             <section className="w-[512px] relative section section--border">
-              <Spline scene="https://prod.spline.design/TM0o-NN7ksLfXyZC/scene.splinecode" />
+              {/* <Spline scene="https://prod.spline.design/TM0o-NN7ksLfXyZC/scene.splinecode" /> */}
+              <Spline scene="https://prod.spline.design/pcINL9diltcqG9xX/scene.splinecode" />
             </section>
 
             <div>
