29340815ac4d63d70adc9776a19de995dafc4fca
Miguel Angel Rivas <miguel.angel.rivas@consid.se>
2024-08-20 22:12:55 +0200

    added animated to home page

--- my-portfolio/src/pages/Home.js
+++ my-portfolio/src/pages/Home.js
@@ -2,10 +2,13 @@
 import { React, useEffect, useState } from "react";
 import data from "../api/data-mock.json";
 import useScrollToTop from "../hooks/useScrollToTop";
+
+
 const HomePage = () => {
   useScrollToTop();
 
   const [animated, setAnimated] = useState(false);
+  
 
   useEffect(() => {
     setTimeout(() => setAnimated(true), 100); // Slight delay to start the animation
@@ -32,20 +35,20 @@ const HomePage = () => {
 
   return (
     <>
-     
       <section className="relative px-8 md:px-0 flex gap-8 m-0 home-section-h pb-8">
+        <div className="animated-gradient grad-filter w-screen h-[1000px] absolute -top-[200px] left-0 z-[-1]"></div>
         <div className="max-w-75 pr-0 md:pr-8 mx-auto mt-8">
-
           <h1 className="text-3xl md:text-6xl font-bold font-bodoni py-5 text-transparent">
             {renderAnimatedText(data?.home.slogan || "")}
           </h1>
-
+          
           <h2 className="text-4xl md:text-9xl font-bold font-bodoni pt-5 pb-10 md:py-20 text-end">
             <i>
               <span className="text-sm">by</span>
             </i>{" "}
             {data?.home.author}
           </h2>
+
           <div
             className="break-words paragraph-style mb-12"
             dangerouslySetInnerHTML={{ __html: data?.home.text }}
